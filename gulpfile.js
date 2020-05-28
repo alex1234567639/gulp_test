9ababa973c58694d5dba2428316a66097cda5fca
alex1234 <alex8603062000@gmail.com>
2020-05-28 14:55:51 +0800

    ftp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var fileinclude = require('gulp-file-include');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-
+//ftp
+var gutil = require( 'gulp-util' );
+var ftp = require( 'vinyl-ftp' );
 
 
 // sass 轉譯
@@ -33,6 +35,33 @@ gulp.task('miniimg', function(){
   .pipe(imagemin())
   .pipe(gulp.dest('dest/images'))
 });
+
+
+//ftp
+gulp.task( 'ftp', ['miniimg'], function () {           //先壓圖在上傳
+ 
+  var conn = ftp.create( {
+      host:     '140.115.236.71',
+      user:     '%ed101+',
+      password: '!654=stu&',
+      parallel: 10,
+  } );
+
+  var globs = [
+      'dest/**',
+      'dest/css/**',
+      'dest/images/**',
+      'index.html'
+  ];
+
+  // using base = '.' will transfer everything to /public_html correctly
+  // turn off buffering in gulp.src for best performance
+
+  return gulp.src( globs, { base: '.', buffer: false } )
+      .pipe( conn.newer( './T2000269' ) ) // only upload newer files
+      .pipe( conn.dest( './T2000269' ) );
+
+} );
 
 //同步
 gulp.task('default', function() {
